24a7ada35fde29889191244526be7752aba4dacc
LoL <zhora.polyakov95@mail.ru>
2021-09-20 15:13:36 +0300

    2nd part

---
+++ 19.py
@@ -0,0 +1,21 @@
+#Найти топ самых дорогих товаров. Кол-во товаров, которые мы ищем, будут задаваться первым аргументом
+from pprint import pprint
+import operator
+
+def bigger_price(limit: int, data: list) -> list:
+    q = []
+    data = sorted(data, key = operator.itemgetter('price'), reverse = True)
+    for i in range(0, limit):
+        q.insert(0, data[i])
+    q = q[::-1]
+    return q
+
+
+# if __name__ == '__main__':
+#     print('Example:')
+#     pprint(bigger_price(2, [
+#         {"name": "bread", "price": 100},
+#         {"name": "wine", "price": 138},
+#         {"name": "meat", "price": 15},
+#         {"name": "water", "price": 1}
+#     ]))
