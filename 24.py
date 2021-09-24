ce05a5ae6fa3611a6274878d52dadfc40765c7fd
LoL <zhora.polyakov95@mail.ru>
2021-09-24 21:54:03 +0300

    2nd part

--- 24.py
+++ 24.py
@@ -1,10 +1,35 @@
 # Отсортируйте данный итератор таким образом, чтобы его элементы оказались в порядке убывания частоты их появления, то есть по количеству раз, которое они появляются в элементах.
 # Если два элемента имеют одинаковую частоту, они должны оказаться в том же порядке, в котором стояли изначально в итераторе.
 import operator
+import pprint
 
 def frequency_sort(items):
-	items.sort(items, key = operator.itemgetter(items))
-	return items
+    unique = []
+
+    for elem in items:
+        if elem not in unique:
+            unique.insert(0, elem)
+
+    unique = unique[::-1]
+    tmp = []
+
+    for elem in unique:
+        unique1 = {}
+        unique1['name'] = elem
+        unique1['count'] = items.count(elem)
+        tmp.insert(0, unique1)
+
+    tmp = tmp[::-1]
+    tmp = sorted(tmp, key = operator.itemgetter('count'), reverse = True)
+    res = []
+
+    for elem in tmp:
+        i = 0
+        while i < elem['count']:
+            res.insert(0, elem['name'])
+            i += 1
+    
+    return res[::-1]
 
 
 if __name__ == '__main__':
